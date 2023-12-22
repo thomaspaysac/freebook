e5876359482034c0bb574ed0cadced7423bb3970
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-12-22 17:01:26 +0100

    Style 'add image' button and add cancel upload function for new posts

--- client/src/Components/Forms/NewPost.jsx
+++ client/src/Components/Forms/NewPost.jsx
@@ -2,9 +2,13 @@ import { useContext, useState } from "react";
 import { authContext } from "../../App";
 // Assets
 import imageUpload from "../../assets/icons/image_upload.png";
+import imageUploadColored from "../../assets/icons/image_upload_colored.png";
+import deleteIcon from "../../assets/icons/delete.png";
+
 
 export const NewPostForm = () => {
   const authData = useContext(authContext);
+  const [fileUploaded, setFileUploaded] = useState(false);
 
   const sendForm = async (e) => {
     e.preventDefault();
@@ -16,15 +20,44 @@ export const NewPostForm = () => {
     })
   }
 
+  const checkFile = (e) => {
+    if (!e.target.value) {
+      setFileUploaded(false);
+    } else {
+      setFileUploaded(true);
+    }
+  }
+
+  const cancelUpload = () => {
+    const fileInput = document.getElementById("file");
+    fileInput.value = null;
+    setFileUploaded(false);
+  }
+
+  const CancelFileUploadButton = () => {
+    if (!fileUploaded) {
+      return null;
+    } else {
+      return (
+        <div className="cancel-upload_button" onClick={cancelUpload}>
+          <img src={deleteIcon} alt="cancel image upload" title="Cancel image upload" />
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="new-post_form">
       <form onSubmit={sendForm}>
         <div className="input-group">
           <textarea name="text"></textarea>
-          <label htmlFor="file">
-            <img src={imageUpload} />
-          </label>
-          <input type="file" name="file" id="file" style={{display: "none"}} />
+          <div className="upload-group">
+            <label htmlFor="file">
+              <img src={fileUploaded ? imageUploadColored : imageUpload} alt='upload image' title="Upload image" />
+            </label>
+            <input type="file" name="file" id="file" style={{display: "none"}} onChange={checkFile} />
+            <CancelFileUploadButton />
+          </div>
         </div>
         <button>Publish post</button>
       </form>
