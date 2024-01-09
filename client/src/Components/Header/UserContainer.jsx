de14f4b3426e6031eaf00d01537b08e336826f26
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2024-01-09 10:04:31 +0100

    Style header's icons

--- client/src/Components/Header/UserContainer.jsx
+++ client/src/Components/Header/UserContainer.jsx
@@ -6,10 +6,17 @@ import { RoundPicture } from "../Images/RoundPicture";
 // Assets
 import homeIcon from "../../assets/icons/home.png";
 import friendsIcon from "../../assets/icons/friends.png";
+import friendsIconHover from "../../assets/icons/friends_hover.png";
 import feedIcon from "../../assets/icons/newspaper.png";
+import feedIconHover from "../../assets/icons/newspaper_hover.png";
+import logoutIcon from "../../assets/icons/logout.png";
+import logoutIconHover from "../../assets/icons/logout_hover.png";
 
 export const UserContainer = () => {
   const [userData, setUserData] = useState(null);
+  const [feedHover, setFeedHover] = useState(false);
+  const [friendsHover, setFriendsHover] = useState(false);
+  const [logoutHover, setLogoutHover] = useState(false);
   const authData = useContext(authContext);
 
   const navigateTo = useNavigate();
@@ -39,18 +46,28 @@ export const UserContainer = () => {
 
   return (
     <nav>
-      {authData.email}
-      <button onClick={() => console.log(userData)}>Get authData</button>
-      <button onClick={logOut}>Log out</button>
-      <Link to='/timeline'>
-        <img src={feedIcon} alt='' />
+      <Link to='/timeline'
+        onMouseEnter={() => setFeedHover(true)}
+        onMouseLeave={() => setFeedHover(false)}
+      >
+        <img src={feedHover ? feedIconHover : feedIcon} title="Feed" alt='' />
       </Link>
-      <Link to='/friends/requests'>
-        <img src={friendsIcon} alt='' />
+      <Link to='/friends/requests'
+        onMouseEnter={() => setFriendsHover(true)}
+        onMouseLeave={() => setFriendsHover(false)}
+      >
+        <img src={friendsHover ? friendsIconHover : friendsIcon} title="Friends requests" alt='' />
       </Link>
       <Link to={`/user/${userData.id}`} className="user-container_profile">
         <RoundPicture source={userData.avatar} radius={'40px'} />
       </Link>
+      <div className="button_logout" 
+        onClick={logOut}
+        onMouseEnter={() => setLogoutHover(true)}
+        onMouseLeave={() => setLogoutHover(false)}
+      >
+        <img src={logoutHover ? logoutIconHover : logoutIcon} title="Logout" alt='logout' />
+      </div>
     </nav>
   )
 }
