de462113f8a04957ad27f7985486d583fb9e28fa
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2024-01-24 09:42:38 +0100

    Translate password recover page

--- client/src/Components/Forms/RecoverForm.jsx
+++ client/src/Components/Forms/RecoverForm.jsx
@@ -1,6 +1,9 @@
-import { supabase } from "../../App"
+import { supabase } from "../../App";
+import { FormattedMessage, useIntl } from "react-intl";
 
 export const RecoverForm = () => {
+  const intl = useIntl();
+
   const sendLink = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -12,11 +15,11 @@ export const RecoverForm = () => {
 
   return (
     <div className="recover-form_container">
-      <h2>Enter your email to reset your password.</h2>
+      <h2><FormattedMessage id="recover_message" defaultMessage="Enter your email to reset your password" /></h2>
       <div className="separator"></div>
       <form onSubmit={sendLink}>
-        <input type="email" name="email" id="email" placeholder="Email"/>
-        <button>Send reset link</button>
+        <input type="email" name="email" id="email" placeholder={intl.formatMessage({ id: "placeholder_email" })}/>
+        <button><FormattedMessage id="recover_link" defaultMessage="Send reset link" /></button>
       </form>
     </div>
   )
