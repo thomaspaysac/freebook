eab75c0ff7e3d33531aa4298f48891bafb5945e7
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2024-01-30 21:29:13 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import './App.css'
 import { createClient } from "@supabase/supabase-js";
 //import { SUPABASE_URL } from './.private';
 //import { SUPABASE_KEY } from './.private';
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
 
 // Pages import
 import { Header } from './Components/Header/Header';
