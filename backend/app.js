3b87aa56b055698c061a5a78592eccda3776dcbe
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2024-01-30 21:02:34 +0100

    Add middleware for production

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const supabase = require('./supabaseConfig')
+// Optimization
+const compression = require("compression");
+const helmet = require("helmet");
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
@@ -15,6 +18,13 @@ const app = express();
 
 app.use(cors());
 
+// Set up rate limiter: maximum of sixty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 60,
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +33,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(limiter);
+app.use(helmet());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
 app.use(async (req, res, next) => {
