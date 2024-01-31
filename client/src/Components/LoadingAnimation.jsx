887690efce95b6974f175fe0bc1ea178431ddc2b
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2024-01-31 07:48:08 +0100

    Update LoadingAnimation.jsx

--- client/src/Components/LoadingAnimation.jsx
+++ client/src/Components/LoadingAnimation.jsx
@@ -14,8 +14,8 @@ export const LoadingAnimation = ({ theme }) => {
 
   return (
     <div className="loader loader--style5" title="4">
-  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     width="24px" height="30px" viewBox="0 0 24 30" style={{enableBackground: "new 0 0 50 50;"}} xmlSpace="preserve">
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+     width="24px" height="30px" viewBox="0 0 24 30" style={{enableBackground: "new 0 0 50 50"}} xmlSpace="preserve">
     <rect x="0" y="0" width="4" height="10" fill={color}>
       <animateTransform attributeType="xml"
         attributeName="transform" type="translate"
